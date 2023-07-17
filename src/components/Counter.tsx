f719341164f4184fc12547b057bd90efb4089581
«Elena <LenaRybinskova@gmail.com>
2023-07-17 19:47:38 +0300

    not work message

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,11 +5,20 @@ import {Setting} from "./Setting";
 import {log} from "util";
 
 
+// сократила кол-в стейтов, данные загр из локалСтор, НО постоянно выдает "enter values and press 'set'"
 const Counter = () => {
     const INITIAL_START_VALUE = 0
     const INITIAL_MAX_VALUE = 5
 
-    const [counter, setCounter] = useState(0)
+    const [display, setDisplay] = useState(() => {
+            let startValueAsString = localStorage.getItem("startValue")
+            if (startValueAsString) {
+                let startValueFromLStorage = JSON.parse(startValueAsString)
+                return startValueFromLStorage
+            } else {
+                return INITIAL_START_VALUE
+            }
+        })
     const [maxValue, setMaxValue] = useState(() => {
         let maxValueAsString = localStorage.getItem("maxValue")
         if (maxValueAsString) {
@@ -28,43 +37,37 @@ const Counter = () => {
             return INITIAL_START_VALUE
         }
     })
-    const [isError, setIsError] = useState(false)
+
     const [textError, setTextError] = useState("")
-    const [disabledSetBtn, setDisabledSetBtn]=useState(true)
-    const [disabledResetBtn, setDisabledResetBtn]=useState(false)
-    const [disabledIncBtn, setDisabledIncBtn]=useState(false)
-    console.log("textError" , textError)
-    console.log("! textError" , !textError)
-    console.log("!! textError" , !!textError)
+    const [disabledSetBtn, setDisabledSetBtn] = useState(true)
+
 
     const startValueHandler = (value: number) => setStartValue(value)
 
     const maxValueHandler = (value: number) => setMaxValue(value)
 
     useEffect(() => {
-        if (maxValue !=INITIAL_MAX_VALUE || startValue !=INITIAL_START_VALUE) {
-            setIsError(true)
+
+
+        if (maxValue != INITIAL_MAX_VALUE || startValue != INITIAL_START_VALUE) {
             setTextError("enter values and press 'set'")
             setDisabledSetBtn(false)
-            setDisabledResetBtn(true)
-            setDisabledIncBtn(true)
+
         }
-        if (maxValue === startValue || maxValue < startValue) {
-            setIsError(true)
+        if (maxValue === startValue || maxValue < startValue || startValue < 0 || maxValue < 0) {
             setTextError("incorrect value!")
-            setDisabledResetBtn(true)
-            setDisabledIncBtn(true)
+            setDisabledSetBtn(true)
         }
     }, [maxValue, startValue])
 
-    const incHandler = () => counter < maxValue && setCounter(counter + 1)
+    const incHandler = () => display < maxValue && setDisplay(display + 1)
 
     const resetBtnHandler = () => {
         let startValueAsString = localStorage.getItem("startValue")
-        if(startValueAsString){
-            setCounter(JSON.parse(startValueAsString))
-        } else{
-            setCounter(0)
+        if (startValueAsString) {
+            setDisplay(JSON.parse(startValueAsString))
+        } else {
+            setDisplay(0)
         }
 
 
@@ -73,31 +76,27 @@ const Counter = () => {
     const setToLocalStorageHandler = () => {
         localStorage.setItem("maxValue", JSON.stringify(maxValue))
         localStorage.setItem("startValue", JSON.stringify(startValue))
-        setIsError(false)
-        setCounter(startValue)
-        setDisabledResetBtn(false)
-        setDisabledIncBtn(false)
+        setDisplay(startValue)
         setTextError("") // если сетнули, текст ошибки обнулился
-
+        setDisabledSetBtn(true)
     }
-
 
     return (
         <div className={s.container}>
             <div>
-                <Setting value={INITIAL_MAX_VALUE} callback={maxValueHandler} name={"maxValue"}/>
-                <Setting value={INITIAL_START_VALUE} callback={startValueHandler} name={"startValue"}/>
+                <Setting value={maxValue} callback={maxValueHandler} name={"maxValue"}/>
+                <Setting value={startValue} callback={startValueHandler} name={"startValue"}/>
                 <Button callback={setToLocalStorageHandler} name={"set"} dis={disabledSetBtn}/>
-            </div>
 
+            </div>
 
             <div>
                 <div>
-                    {isError ? <div>{textError}</div> : <div>{counter}</div>}
+                    {textError ? <div>{textError}</div> : <div>{display}</div>}
                 </div>
                 <div>
-                    <Button callback={incHandler} name={"inc"} dis={counter===maxValue || disabledIncBtn}/>
-                    <Button callback={resetBtnHandler} name={"reset"} dis={disabledResetBtn}/>
+                    <Button callback={incHandler} name={"inc"} dis={display === maxValue || !!textError}/>
+                    <Button callback={resetBtnHandler} name={"reset"} dis={!!textError}/>
                 </div>
             </div>
 
